6a4f791fa14ba86e1a2118835df309ee48fe8f06
Heather Swope <hswope@MacBook-Pro-van-Heather.local>
2020-04-26 19:25:58 +0200

    Register/Deregister printer

--- app/js/app.js
+++ app/js/app.js
@@ -38,6 +38,8 @@ window.addEventListener('load', async function() {
         // We wire it when the system looks in order.
         $("#registerDesigner").click(registerDesigner);
         $("#deregisterDesigner").click(deregisterDesigner);
+        $("#registerPrinter").click(registerPrinter);
+        $("#deregisterPrinter").click(deregisterPrinter);
        // $("#withdrawFunds").click(withdrawFunds);
         
     } catch(err) {
@@ -157,6 +159,129 @@ const deregisterDesigner = async function() {
         } else {
             console.log("logs ", receipt.logs[0]);
             $("#status").html("Designer deregistered");
+        }
+        
+        // Make sure we update the UI.
+       // $("#balanceContract").html(await web3.eth.getBalance(instance.address));
+
+    } catch(err) {
+        $("#status").html(err.toString());
+        console.error(err);
+    }
+};
+
+const registerPrinter = async function() {
+    // Sometimes you have to force the gas amount to a value you know is enough because
+    // `web3.eth.estimateGas` may get it wrong.
+    const gas = 300000;
+    try {
+        const accounts = await (/*window.ethereum ?
+            window.enable() ||*/
+            web3.eth.getAccounts());
+            console.log("accounts ", accounts);
+        const instance = await IPProtection.deployed();
+
+        // We simulate the real call and see whether this is likely to work.
+        // No point in wasting gas if we have a likely failure.
+        const success = await instance.registerPrinter.call(
+            $("input[name='printerAddress']").val(),
+            { from: window.account, gas: gas });
+
+        if (!success) {
+            throw new Error("The transaction will fail anyway, not sending");
+        }
+
+        // Ok, we move onto the proper action.
+        const txObj = await instance.registerPrinter(
+            $("input[name='printerAddress']").val(),
+            { from: window.account, gas: gas })
+            //transfer takes time in real life, so we get the txHash immediately while it 
+            // is mined.
+            .on(
+                "transactionHash",
+                txHash => $("#status").html("Transaction on the way " + txHash)
+            )
+            .on('receipt', function(receipt){
+                console.log("receipt in on receipt ", receipt);
+                console.log("events in on receipt ", receipt.events);
+
+            });
+        // Now we got the mined tx.
+        const receipt = txObj.receipt;
+
+        if (!receipt.status) {
+            console.error("Wrong status");
+            console.error(receipt);
+            $("#status").html("There was an error in the tx execution, status not 1");
+        } else if (receipt.logs.length == 0) {
+            console.error("Empty logs");
+            console.error(receipt);
+            $("#status").html("There was an error in the tx execution, missing expected event");
+        } else {
+            console.log("logs ", receipt.logs[0]);
+            $("#status").html("Printer registered");
+        }
+        
+        // Make sure we update the UI.
+       // $("#balanceContract").html(await web3.eth.getBalance(instance.address));
+
+    } catch(err) {
+        $("#status").html(err.toString());
+        console.error(err);
+    }
+};
+
+
+const deregisterPrinter = async function() {
+    // Sometimes you have to force the gas amount to a value you know is enough because
+    // `web3.eth.estimateGas` may get it wrong.
+    const gas = 300000;
+    try {
+        const accounts = await (/*window.ethereum ?
+            window.enable() ||*/
+            web3.eth.getAccounts());
+            console.log("accounts ", accounts);
+        const instance = await IPProtection.deployed();
+
+        // We simulate the real call and see whether this is likely to work.
+        // No point in wasting gas if we have a likely failure.
+        const success = await instance.deregisterPrinter.call(
+            $("input[name='printerAddress']").val(),
+            { from: window.account, gas: gas });
+
+        if (!success) {
+            throw new Error("The transaction will fail anyway, not sending");
+        }
+
+        // Ok, we move onto the proper action.
+        const txObj = await instance.deregisterPrinter(
+            $("input[name='printerAddress']").val(),
+            { from: window.account, gas: gas })
+            //transfer takes time in real life, so we get the txHash immediately while it 
+            // is mined.
+            .on(
+                "transactionHash",
+                txHash => $("#status").html("Transaction on the way " + txHash)
+            )
+            .on('receipt', function(receipt){
+                console.log("receipt in on receipt ", receipt);
+                console.log("events in on receipt ", receipt.events);
+
+            });
+        // Now we got the mined tx.
+        const receipt = txObj.receipt;
+
+        if (!receipt.status) {
+            console.error("Wrong status");
+            console.error(receipt);
+            $("#status").html("There was an error in the tx execution, status not 1");
+        } else if (receipt.logs.length == 0) {
+            console.error("Empty logs");
+            console.error(receipt);
+            $("#status").html("There was an error in the tx execution, missing expected event");
+        } else {
+            console.log("logs ", receipt.logs[0]);
+            $("#status").html("Printer deregistered");
         }
         
         // Make sure we update the UI.
